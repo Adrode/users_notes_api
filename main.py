bc19dab1253fd623ecdb2f837e923b766706080a
Adrian Wozniak <adrianwozniak20@gmail.com>
2026-02-05 19:46:39 +0100

    feature | get and post endpoints for Notes

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from models import Base, User, Note
 from database import engine, get_db
+from schemas import CreateUser, CreateNote, UpdateNoteContent
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# USERS ENDPOINTS
 
 @app.get("/users")
 def get_users(db: Session = Depends(get_db)):
@@ -15,10 +18,25 @@ def get_users(db: Session = Depends(get_db)):
   return users
 
 @app.post("/users")
-def add_user(email: str, name: str, db: Session = Depends(get_db)):
-  new_user = User(email=email, name=name)
+def add_user(create_user: CreateUser, db: Session = Depends(get_db)):
+  new_user = User(email=create_user.email, name=create_user.name)
   db.add(new_user)
   db.commit()
   db.refresh(new_user)
   return new_user
 # TO DO LATER: exception_handler na wypadek podania danych, które nie mogą zostać dodane do tabeli ze względu na unique albo inny contstraint
+
+# NOTES ENDPOINTS
+
+@app.get("/notes")
+def get_notes(db: Session = Depends(get_db)):
+  notes = db.query(Note).all()
+  return notes
+
+@app.post("/notes")
+def add_note(create_note: CreateNote, db: Session = Depends(get_db)):
+  new_note = Note(title=create_note.title, content=create_note.content, user_id=create_note.user_id)
+  db.add(new_note)
+  db.commit()
+  db.refresh(new_note)
+  return new_note
--- schemas.py
+++ schemas.py
@@ -1,1 +1,16 @@
 # PYDANTIC SCHEMAS
+
+from pydantic import BaseModel
+from typing import Optional
+
+class CreateUser(BaseModel):
+  email: str
+  name: str
+
+class CreateNote(BaseModel):
+  title: str
+  content: Optional[str] = None
+  user_id: Optional[int] = None
+
+class UpdateNoteContent(BaseModel):
+  content: Optional[str] = None
