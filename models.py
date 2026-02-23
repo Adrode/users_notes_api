4ff3c01318fb59ad564753d5c255267fc7809582
Adrian Wozniak <adrianwozniak20@gmail.com>
2026-02-23 18:03:30 +0100

    fix | ondelete handling

--- models.py
+++ models.py
@@ -10,13 +10,13 @@ class User(Base):
   email = Column(String, nullable=False, unique=True)
   name = Column(String, nullable=False)
 
-  notes = relationship('Note', back_populates='user')
+  notes = relationship('Note', back_populates='user', passive_deletes=True)
 
 class Note(Base):
   __tablename__ = 'notes'
   id = Column(Integer, primary_key=True)
   title = Column(String, nullable=False)
   content = Column(String, nullable=True)
-  user_id = Column(Integer, ForeignKey('users.id', ondelete="SET NULL"), nullable=True)
+  user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"))
 
   user = relationship('User', back_populates='notes')
